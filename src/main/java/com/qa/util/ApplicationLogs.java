27e59ef060a301088b204ed85b804f15bfbbf4c0
mnukala29 <mnukala29@gmail.com>
2018-07-30 10:21:33 +0530

    Added the logs class and jar

---
+++ src/main/java/com/qa/util/ApplicationLogs.java
@@ -0,0 +1,15 @@
+package com.qa.util;
+
+import org.apache.log4j.Logger;
+
+public class ApplicationLogs {
+
+	public static void main(String[] args) {
+		
+		Logger log=Logger.getLogger(ApplicationLogs.class);
+		log.info("About to launch browser");
+		
+	
+	}
+
+}
